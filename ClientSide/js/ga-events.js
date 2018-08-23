9533ace606639d9268bcbaeb11491712a6aa9bff
Alejandro <alejogtunit460@gmail.com>
2018-08-22 23:00:18 -0600

    ga events

---
+++ ClientSide/js/ga-events.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#btn-filtro-todos-desk, #")
+    
+    $("#select-filtros-mobile").on('change', function()
+    {
+        filtroEventos = $("#select-filtros-mobile").val();
+        
+            
+    });
+});
+
